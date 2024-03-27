50eda04949dc69599d4564b105b80e02f425e6c0
unknown <tatsiana.krauchanka@gmail.com>
2024-03-28 01:03:38 +0300

    feat: complete getAllResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -115,8 +115,19 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const arrOfResults = [];
+
+  return new Promise((resolve) => {
+    promises.forEach((item) =>
+      item.then(
+        (result) => arrOfResults.push(result),
+        () => arrOfResults.push(null)
+      )
+    );
+
+    resolve(arrOfResults);
+  });
 }
 
 /**
